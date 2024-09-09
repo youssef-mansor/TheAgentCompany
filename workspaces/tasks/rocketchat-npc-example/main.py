ee9edad39dda58ee4dcdbf2715665afec200976c
Yufan Song <33971064+yufansong@users.noreply.github.com>
2024-09-09 00:40:40 -0700

    refactor credential out (#107)

--- workspaces/tasks/rocketchat-npc-example/main.py
+++ workspaces/tasks/rocketchat-npc-example/main.py
@@ -1,16 +1,28 @@
 import asyncio
 from server import run_server
+import argparse
 
-asyncio.run(
-    run_server(
-        model_dict={
-            "env": "gpt-4-turbo",
-            "agent1": "gpt-4-turbo",
-            "agent2": "rocketchat",
-        },
-        agents_roles={
-            "agent1": "human",
-            "agent2": "ai",
-        },
+def main():
+    # Use argparse to capture command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--agent_first_name', type=str, help="Input NPC first name")
+    args = parser.parse_args()
+
+    # Run the asyncio task
+    asyncio.run(
+        run_server(
+            model_dict={
+                "env": "gpt-4-turbo",
+                "agent1": "gpt-4-turbo",
+                "agent2": "rocketchat",
+            },
+            agents_roles={
+                "agent1": "human",
+                "agent2": "ai",
+            },
+            agent_first_name = args.agent_first_name
+        )
     )
-)
+
+if __name__ == "__main__":
+    main()
