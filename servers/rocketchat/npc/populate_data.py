4842a77ee1da0307ed1e03c56196f59f63f879fd
Boxuan Li <liboxuan@connect.hku.hk>
2024-09-11 23:13:45 -0700

    NPC: Code clean up and doc polish (#118)
    * Polish NPC example doc
    
    * Populate NPC definitions from npc_definition.json
    
    * Fix NPC_CONFIG.md
    
    * Emphasize that NPC definitions are shared across tasks

--- servers/rocketchat/npc/populate_data.py
+++ servers/rocketchat/npc/populate_data.py
@@ -1,40 +1,23 @@
-from redis_om import JsonModel, Migrator
+import json
+import logging
+
+from redis_om import Migrator
 from sotopia.database import AgentProfile
 
 Migrator().run()
 
-
-agent_definitions = [
-    {
-        "first_name": "X",
-        "last_name": "AI",
-        "occupation": "AI Assistant",
-    },
-    {
-        "first_name": "John",
-        "last_name": "Doe",
-        "age": 30,
-        "occupation": "Software Engineer",
-    },
-    {
-        "first_name": "rocket",
-        "last_name": "cat",
-        "age": 30,
-        "occupation": "Software Engineer",
-    }
-]
-
+with open('npc_definition.json', 'r') as file:
+    agent_definitions = json.load(file)
+    logging.info(f"NPC definitions loaded, number of NPCs = {len(agent_definitions)}")
 
 def get_by_first_name(first_name):
     return AgentProfile.find(AgentProfile.first_name == first_name).execute()
 
 for definition in agent_definitions:
     if get_by_first_name(definition["first_name"]):
-        print("Already inserted, skip")
+        # TODO: shall we support modifications?
+        logging.info(f'NPC ({definition["first_name"]}) already inserted, skip')
         continue
     agent_profile = AgentProfile.parse_obj(definition)
     agent_profile.save()
-    print("Inserted ", definition["first_name"] ," successfully")
-
-for definition in agent_definitions:
-    print(get_by_first_name(definition["first_name"]))
+    logging.info(f'Inserted {definition["first_name"]} successfully')
