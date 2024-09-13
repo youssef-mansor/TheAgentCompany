f302d5392634b494ff1f2bc85f6fd94daa9e89a7
yufansong <yufansong98@gmail.com>
2024-09-13 15:04:37 -0400

    comment out pre init.py

--- workspaces/tasks/example/pre_init.py
+++ workspaces/tasks/example/pre_init.py
@@ -76,12 +76,13 @@ def can_access_website(url):
 # test mock local API
 # test prepared file
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
     # TODO: polish this file so that people could use it to testify the prerequisite
     # The function mainly test 2 parts:
     #     1. Local environment: not very important, because we always can quickly restart the task image to fresh
     #     2. Sever environment: important, if the test fails, that means user need to reset the server image
     # This file should write by benchmark developer and optionally used by benchmark user
-    test_pull_repository()
-    test_wiki()
-    test_black_white_list()
+    # test_pull_repository()
+    # test_wiki()
+    # test_black_white_list()
+    
