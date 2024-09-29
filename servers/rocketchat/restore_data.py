6d521aa4b0debf9783e0ee664e1662ca5b07c4f1
Yufan Song <33971064+yufansong@users.noreply.github.com>
2024-09-29 02:16:50 -0700

    feature: add rocketchat restore code (#156)
    * add rocketchat restore code
    
    * remove

---
+++ servers/rocketchat/restore_data.py
@@ -0,0 +1,23 @@
+import time
+import requests
+import os
+
+url = "http://localhost:3000"
+
+def wait_for_rocketchat(retries=300, delay=3):
+    while True:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                print("Web service is up!")
+                break
+            else:
+                print(f"Web service returned status code {response.status_code}. Waiting...")
+        except requests.ConnectionError:
+            print("Web service is not available yet. Retrying...")
+        time.sleep(delay)
+
+
+wait_for_rocketchat()
+
+os.system("make restore-rocketchat")
