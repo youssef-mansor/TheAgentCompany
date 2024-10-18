cb0d2b2c94c7b41a2e083b62d04e4442dcbddc4a
AndyLu98 <37010236+AndyLu98@users.noreply.github.com>
2024-10-17 23:44:18 -0500

    TASK: collecting feedback (#272)
    * task for collecting performance feedback
    ---------
    
    Co-authored-by: Andy Lu <andylu@Mac.home>

---
+++ workspaces/tasks/hr-collect-feedbacks/evaluator.py
@@ -0,0 +1,77 @@
+import os
+from rocketchat_API.rocketchat import RocketChat
+
+from llm_evaluator import *
+
+############################# Init Variables #####################################
+# Rocket.Chat variables
+SERVER_HOSTNAME = os.getenv('SERVER_HOSTNAME') or 'ogma.lti.cs.cmu.edu'
+
+
+ROCKETCHAT_PORT = os.getenv('ROCKETCHAT_PORT') or '3000'
+
+
+ROCKETCHAT_URL = f"http://{SERVER_HOSTNAME}:{ROCKETCHAT_PORT}"
+ADMIN_USERNAME = 'jobbench'
+ADMIN_PASS = 'jobbench'
+
+
+# Initialize RocketChat client
+rocket = RocketChat(ADMIN_USERNAME, ADMIN_PASS, server_url=ROCKETCHAT_URL)
+
+############################# Helper Functions #####################################
+
+def get_history(username: str):
+    id = ""
+    for item in rocket.users_list().json()['users']:
+        if item['nameInsensitive'] == username.lower():
+            id = item["_id"]
+            break
+    msgs = rocket.im_history(room_id=id).json()['messages']
+    for msg in msgs[::-1]:
+        print(f"{username}'s chat: {msg['msg']}")
+
+    if len(msgs)>0:
+        return True, msg
+    else:
+        return False, msg
+
+
+def check_final_feedback(username: str):
+
+    _, msg = get_history(username)
+
+    messages = [{"content": f"Does the phrase \"\"{msg}\"\" indicate the person receiving the feedback had an average job performance? Please answer 'yes' if it does, or 'no' if it doesn't.", "role": "user"}]
+    # Check result
+    llm_resonse = llm_evaluator(messages).json()
+    print(llm_resonse)
+
+    if 'yes' in llm_resonse['choices'][0]['message']['content'].lower():
+        print(f"{username} received feedback that indicates an average job performance")
+        return True
+    else:
+        print(f"{username} did not receive feedback that indicates an average job performance")
+        return False
+
+############################# Evaluator #####################################
+
+def evaluate_task():
+    points = 0
+
+    npcs = ["Mike Chen", "Li Ming", "Zhang Wei"]
+    performance_evaluatee = "Liu Qiang"
+
+    for npc in npcs:
+        res, _ = get_history(npc)
+        if res:
+            points += 1
+
+    if check_final_feedback(performance_evaluatee):
+        points += 2
+
+    # Final result
+    print(f"Evaluation completed. Final score: {points}/5")
+    return points
+
+if __name__ == "__main__":
+    evaluate_task()
