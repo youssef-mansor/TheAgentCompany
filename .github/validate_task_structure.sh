cf4c1ef5b6f03d5feb259b5c7902d693526ff99e
Boxuan Li <liboxuan@connect.hku.hk>
2024-10-22 23:37:47 -0700

    CI: Use self-hosted runners (#345)
    * CI: Use self-hosted runners
    
    * Add pipx install
    
    * Add cancel-in-progress
    
    * Remove build-task-image which is covered in test-evaluator-basic
    
    * Adhoc fixes to enable CI on self-hosted runners
    
    * Add workaround
    
    * Bump fixed pandas version to avoid building from wheel

--- .github/validate_task_structure.sh
+++ .github/validate_task_structure.sh
@@ -72,7 +72,12 @@ for task_dir in *; do
     exit 1
   fi
 
-  poetry run python ../../../.github/validate_dependencies.py "dependencies.yml"
+  # self-hosted runner doesn't have poetry environment
+  # poetry run python ../../../.github/validate_dependencies.py "dependencies.yml"
+  ##### TEMPORARY WORKAROUND FOR SELF-HOSTED RUNNER #####
+  pip install pyyaml
+  python ../../../.github/validate_dependencies.py "dependencies.yml"
+  ##### TEMPORARY WORKAROUND FOR SELF-HOSTED RUNNER #####
 
   cd -
 done
