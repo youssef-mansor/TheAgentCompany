140fbb8d99a73cb68e59ff0fe80f4b430aa324a2
Yufan Song <33971064+yufansong@users.noreply.github.com>
2024-10-29 20:50:40 -0700

    Task: change repository license easy (#463)
    * add init code
    
    * fix bug
    
    * Update workspaces/tasks/sde-change-license-easy/checkpoints.md
    
    * Update workspaces/tasks/sde-change-license-easy/task.md
    
    * Update workspaces/base_image/common.py
    
    * optimzie
    
    * Update workspaces/tasks/sde-change-license-easy/evaluator.py
    
    Co-authored-by: Boxuan Li <liboxuan@connect.hku.hk>
    
    * Update workspaces/tasks/sde-change-license-easy/task.md
    
    Co-authored-by: Boxuan Li <liboxuan@connect.hku.hk>
    
    ---------
    
    Co-authored-by: Boxuan Li <liboxuan@connect.hku.hk>

---
+++ workspaces/tasks/sde-change-license-easy/checkpoints.md
@@ -0,0 +1,17 @@
+# Checkpoints
+
+This task has 4 points in total. If the last check passes, then full credits are
+granted. Otherwise, partial credits might be granted if only some checkpoints are
+completed.
+
+## Checkpoint 1 (1pt)
+
+Get the correct codebase http://the-agent-company.com:8929/root/janusgraph and go to the license to edit the license of the repository by keeping the APACHE-2.0 license and removing the CC-BY-4.0 license.
+
+## Checkpoint 2 (1pt)
+
+Clone the repository locally.
+
+## Checkpoint 3 (2pt)
+
+Check the license of the repository.
---
+++ workspaces/tasks/sde-change-license-easy/evaluator.py
@@ -0,0 +1,38 @@
+import logging
+from common import check_repo_exists
+from scoring import Result, Checkpoint, bonus_for_completing_final
+
+def grade_checkpoint1(trajectory):
+    return (
+        "/root/janusgraph" and "/root/janusgraph/-/blob/main/LICENSE"
+        in trajectory
+    )
+
+def grade_checkpoint2():
+    return check_repo_exists("janusgraph")
+
+def grade_checkpoint3():
+    try:
+        with open("/workspace/janusgraph/LICENSE.txt") as f:
+            code_content = f.read()
+            if "APACHE-2.0" in code_content and "CC-BY-4.0" not in code_content:
+                return True
+    except Exception as e:
+        logging.warning(f"Error checking license: {e}")
+    return False
+
+
+def grade_checkpoints(trajectory="") -> Result:
+    checkpoints: List[Checkpoint] = []
+    result = Result(checkpoints, bonus_for_completing_final)
+
+    passed1 = grade_checkpoint1(trajectory)
+    checkpoints.append(Checkpoint(1, int(passed1)))
+
+    passed2 = grade_checkpoint2()
+    checkpoints.append(Checkpoint(1, int(passed2)))
+
+    passed3 = passed2 and grade_checkpoint3()
+    checkpoints.append(Checkpoint(2, 2 * int(passed3)))
+
+    return result
