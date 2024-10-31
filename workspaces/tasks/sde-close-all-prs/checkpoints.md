05332d2f9215601e2fdb265a862070979300b18e
Murong Cao <apyanhui@gmail.com>
2024-10-31 15:15:25 +0800

    SDE Task: close all PRs in all repos (#493)
    * SDE Task: close all PRs in all repos
    
    * Fix

---
+++ workspaces/tasks/sde-close-all-prs/checkpoints.md
@@ -0,0 +1,7 @@
+# Checkpoints
+
+This task has 2 points in total. 
+
+## Checkpoint 1 (2pts)
+
+All PRs in all projects are closed.
---
+++ workspaces/tasks/sde-close-all-prs/evaluator.py
@@ -0,0 +1,38 @@
+import logging
+from typing import List
+
+from scoring import Result, Checkpoint
+from common import *
+
+
+def grade_checkpoint1():
+    try:
+        # Get all projects
+        response = make_gitlab_request(additional_path="projects").json()
+
+        if len(response) == 0:
+            logging.warning(f"All repositories are deleted.")
+            return False
+
+        for repo in response:
+            repo_name = repo["name"]
+            repo_id = str(repo["id"])
+            merge_requests = make_gitlab_request(project_identifier=repo_id, additional_path="merge_requests", params={"state": "opened"}).json()
+            if len(merge_requests) > 0:
+                logging.warning(f"Some PRs are not closed in repository {repo_name}.")
+                return False
+
+        return True
+
+    except Exception as e:
+        logging.error(f"Error occurred while checking the repository", exc_info=True)
+        return False
+
+
+def grade_checkpoints(trajectory="") -> Result:
+    checkpoints: List[Checkpoint] = []
+    result = Result(checkpoints)
+
+    checkpoints.append(Checkpoint(2, 2 * int(grade_checkpoint1())))
+
+    return result
