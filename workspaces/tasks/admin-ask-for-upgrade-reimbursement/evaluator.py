c9f0a31d4981eaa04b72a198b5b81d5a86a1ca75
Zora/Zhiruo Wang <zhiruo.zora.wang@gmail.com>
2024-11-02 03:23:08 -0400

    Administrative Specialist Task: ask for upgrade reimbursement (#480)
    * add task files
    
    * fix value
    
    * move bill in images to next cloud
    
    * make eval more robust with llm eval
    
    * refine task desc
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Boxuan Li <liboxuan@connect.hku.hk>

---
+++ workspaces/tasks/admin-ask-for-upgrade-reimbursement/evaluator.py
@@ -0,0 +1,29 @@
+
+from typing import List
+
+from scoring import Result, Checkpoint
+from common import create_rocketchat_client
+from common import get_chat_history, evaluate_with_llm
+
+# Initialize RocketChat client
+rocket = create_rocketchat_client()
+
+def grade_checkpoints(trajectory=""):
+    checkpoints: List[Checkpoint] = []
+    result = Result(checkpoints)
+
+    chat_history = get_chat_history(rocket, "Chen Xinyi")
+    checkpoints.append(Checkpoint(1, int(len(chat_history) > 0)))
+
+    chat_history = get_chat_history(rocket, "Sarah Johnson")
+    checkpoints.append(Checkpoint(1, int(len(chat_history) > 0)))
+
+    mentioned_price = "365" in ' '.join(chat_history)
+    if mentioned_price:
+        score = 2
+    if not mentioned_price:
+        fuzzy_mention = evaluate_with_llm(str(chat_history), "someone asks if they can get reimbursed for half of the bill, which is $365")
+        score = int(fuzzy_mention)
+    checkpoints.append(Checkpoint(2, score))    
+
+    return result
