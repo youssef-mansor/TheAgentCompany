7e64237c9fba53008e016d0ed25b44f6d9ad848f
Zora/Zhiruo Wang <zhiruo.zora.wang@gmail.com>
2024-11-02 16:52:09 -0400

    Administrative Specialist Task: not qualified bill ask for reimburse (#483)
    * add task files
    
    * upload bill to nextcloud and modify task
    
    * Update policy file path
    
    Co-authored-by: Boxuan Li <liboxuan@connect.hku.hk>
    
    * remove comments
    
    * clarify bill content in checkpoints.md
    
    ---------
    
    Co-authored-by: Boxuan Li <liboxuan@connect.hku.hk>

---
+++ workspaces/tasks/admin-nonqualified-bill-ask-for-reimburse/evaluator.py
@@ -0,0 +1,48 @@
+import requests
+import os
+
+from typing import List
+
+from scoring import Result, Checkpoint, bonus_for_completing_final
+
+
+def grade_checkpoint1(trajectory):
+    policy_file_path = "apps/files/files/1363?dir=/Documents/Administrative%20Specialist"
+    return policy_file_path in trajectory
+
+def grade_checkpoint2():
+    path = "/workspace/reimburse.txt"
+    if not os.path.exists(path):
+        return False
+    text = open(path, 'r').read()
+    if any([word in text for word in ["85.6", "25.0", "$"]]):
+        return False
+    return True
+
+
+def grade_final_checkpoint():
+    path = "/workspace/reimburse.txt"
+    if not os.path.exists(path):
+        return False
+    text = open(path, 'r').read()
+    if all([phrase.lower() in text.lower() for phrase in [
+        "not qualified", "Credit card statements not accepted as receipts"
+    ]]):
+        return True
+    return False
+
+
+def grade_checkpoints(trajectory="") -> Result:
+    checkpoints: List[Checkpoint] = []
+    result = Result(checkpoints, bonus_for_completing_final)
+
+    passed1 = grade_checkpoint1(trajectory)
+    checkpoints.append(Checkpoint(1, int(passed1)))
+
+    passed2 = grade_checkpoint2()
+    checkpoints.append(Checkpoint(1, int(passed2)))
+
+    passed3 = grade_final_checkpoint()
+    checkpoints.append(Checkpoint(1, int(passed3)))
+
+    return result
