51513626d1c4ea63814d31502fb0c31d25ee57f2
Zora/Zhiruo Wang <zhiruo.zora.wang@gmail.com>
2024-11-02 13:59:08 -0400

    Admin Specialist Task: ask for feedback and score about the meeting (#484)
    * add task files
    
    * fix evaluator with try catch
    
    * remove data
    
    * specify library versions
    
    * remove comments
    
    * specify task desc

---
+++ workspaces/tasks/admin-ask-for-meeting-feedback/evaluator.py
@@ -0,0 +1,67 @@
+import os
+import pandas as pd
+
+from typing import List
+from scoring import Result, Checkpoint, bonus_for_completing_final
+from common import create_rocketchat_client, get_chat_history
+
+rocket = create_rocketchat_client()
+
+
+def grade_checkpoint1():
+    chat = get_chat_history(rocket, "Huang Jie")
+    return len(chat) > 0
+
+
+def grade_checkpoint2():
+    return os.path.exists('/workspace/meeting_feedback.xlsx')
+
+
+def grade_checkpoint3():
+    if not os.path.exists("/workspace/meeting_feedback.xlsx"):
+        return False
+    df = pd.read_excel("/workspace/meeting_feedback.xlsx")
+    return sorted(df["Name"].tolist()) == ['Chen Xinyi', 'Emily Zhou', 'Huang Jie', 'Liu Qiang', 'Mike Chen']
+
+
+def grade_checkpoint4():
+    if not os.path.exists("/workspace/meeting_feedback.xlsx"):
+        return False
+    df = pd.read_excel("/workspace/meeting_feedback.xlsx")
+    row = df[df["Name"]=="Chen Xinyi"]
+    return (pd.isna(row["Score"]).tolist()[0]) and (pd.isna(row["Feedback"]).tolist()[0])
+
+def grade_checkpoint5():
+    if not os.path.exists("/workspace/meeting_feedback.xlsx"):
+        return False
+    df = pd.read_excel("/workspace/meeting_feedback.xlsx")
+    others_df = df[df["Name"] != "Chen Xinyi"]
+    others_feedback = others_df["Feedback"].tolist()
+    if not all(["helpful" in feedback for feedback in others_feedback]):
+        return False
+    others_scores = others_df["Score"].tolist()
+    if min(others_scores) < 4:
+        return False
+    return True
+
+
+def grade_checkpoints(trajectory="") -> Result:
+    checkpoints: List[Checkpoint] = []
+    result = Result(checkpoints, bonus_for_completing_final)
+
+    passed1 = grade_checkpoint1()
+    checkpoints.append(Checkpoint(1, int(passed1)))
+
+    passed2 = grade_checkpoint2()
+    checkpoints.append(Checkpoint(1, int(passed2)))
+
+    passed3 = grade_checkpoint3()
+    checkpoints.append(Checkpoint(1, int(passed3)))
+    
+    passed4 = grade_checkpoint4()
+    checkpoints.append(Checkpoint(1, int(passed4)))
+
+    passed5 = grade_checkpoint5()
+    checkpoints.append(Checkpoint(1, int(passed5)))
+
+    return result
