9327b139212e8c372470bb16964707cde295f484
Zhe Su <360307598@qq.com>
2024-11-02 23:39:00 -0400

    add sde-find-answer-in-codebase 2 (#532)
    * add sde-find-answer-in-codebase 2
    
    * update descriptions and evaluation method
    
    ---------
    
    Co-authored-by: Boxuan Li <liboxuan@connect.hku.hk>

---
+++ workspaces/tasks/sde-find-answer-in-codebase-2/checkpoints.md
@@ -0,0 +1,6 @@
+# checkpoints
+
+This task has 3 points in total.
+
+## Checkpoint 1 (3pt)
+Check if the correct PR number (#9711) in `llama.cpp` (http://the-agent-company.com:8929/root/llama.cpp) exists in the chat with Chen Xinyi.
---
+++ workspaces/tasks/sde-find-answer-in-codebase-2/evaluator.py
@@ -0,0 +1,35 @@
+import logging
+import json
+from typing import List
+from scoring import Result, Checkpoint
+from common import create_rocketchat_client, get_rocketchat_personal_chat_history
+
+
+# Create RocketChat instance
+rocket = create_rocketchat_client()
+
+
+def grade_checkpoint1():
+    # [3pts] Check if the correct PR number (#9711) in `llama.cpp` (http://the-agent-company.com:8929/root/llama.cpp) exists in the chat with Chen Xinyi.
+
+    try:
+        messages = get_rocketchat_personal_chat_history(rocket, "Chen Xinyi")
+        get_correct_pr = ("9711" in str(messages).lower())
+
+        if not get_correct_pr:
+            logging.error(f"The examinee does not mention the correct PR number")
+            return False
+        return True
+    except Exception as e:
+        logging.error(f"Error fetching message data or evaluating: {e}")
+        return False
+
+
+def grade_checkpoints(trajectory="") -> Result:
+    checkpoints: List[Checkpoint] = []
+    result = Result(checkpoints)
+
+    passed1 = grade_checkpoint1()
+    checkpoints.append(Checkpoint(3, 3 * int(passed1)))
+
+    return result
