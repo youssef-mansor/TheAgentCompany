169c6959456f40d150dd1f6614863f6b1f015b93
Xuhui Zhou <zhouxuhui2018@gmail.com>
2024-11-04 00:20:18 -0500

    [Task] Ds/find meeting spreasheet (#551)
    * find meeting sheet
    
    * update eval
    
    * remove depents
    
    * correct format
    
    * update make file
    
    * Update workspaces/tasks/ds-find-meeting-spreadsheet/evaluator.py
    
    ---------
    
    Co-authored-by: Boxuan Li <liboxuan@connect.hku.hk>

---
+++ workspaces/tasks/ds-find-meeting-spreadsheet/checkpoints.md
@@ -0,0 +1,11 @@
+# Checkpoints
+
+This task has 2 points in total.
+
+## Checkpoint 1 (1pts)
+
+The agent includes a `link.txt` file under `/workspace/` with the correct file link. (i.e., https://ogma.lti.cs.cmu.edu/apps/files/files/1447?dir=/Documents/Data%20Analysis&openfile=true)
+
+## Checkpoint 2 (1pts)
+
+The agent successfully downloads the target file called `Seed Area Estimates.xlsx` under `/workspace/`.
---
+++ workspaces/tasks/ds-find-meeting-spreadsheet/evaluator.py
@@ -0,0 +1,53 @@
+import logging
+import os
+
+from typing import List
+
+from scoring import Result, Checkpoint
+from common import *
+
+def grade_checkpoint1():
+    try:
+        with open('/workspace/link.txt', 'r') as file:
+            link = file.read().strip()
+            if link is not None and "apps/files/files/1447?dir=/Documents/Data%20Analysis" in link:
+                logging.info("Correct link found in link.txt.")
+                return True
+            else:
+                logging.warning("Incorrect link found in link.txt.")
+                return False
+    except FileNotFoundError:
+        logging.error("File link.txt not found.")
+        return False
+    except Exception as e:
+        logging.error(f"Error occurred while checking link.txt: {e}")
+        return False
+
+def grade_checkpoint2():
+    try:
+        if os.path.isfile('/workspace/Seed Area Estimates.xlsx'):
+            logging.info("File 'Seed Area Estimates.xlsx' successfully downloaded.")
+            return True
+        else:
+            logging.warning("File 'Seed Area Estimates.xlsx' not found.")
+            return False
+    except Exception as e:
+        logging.error(f"Error occurred while checking the file: {e}")
+        return False
+
+
+def grade_checkpoints(trajectory="") -> Result:
+    checkpoints: List[Checkpoint] = []
+    result = Result(checkpoints)
+
+    # Checkpoint 1: Clone the repository
+    passed_checkpoint1 = grade_checkpoint1()
+    checkpoints.append(Checkpoint(1, int(passed_checkpoint1)))
+
+    # Checkpoint 2: create branch
+    passed_checkpoint2 = grade_checkpoint2()
+    checkpoints.append(Checkpoint(1, int(passed_checkpoint2)))
+
+    return result
+
+
