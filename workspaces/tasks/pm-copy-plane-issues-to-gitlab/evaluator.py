fa6eb28ea44e81d54d4a8555d5b9c27a047bdc49
Amaad Martin <57241464+AmaadMartin@users.noreply.github.com>
2024-11-04 21:41:45 -0500

    Pm copy plane issues to gitlab (#556)
    * created pm-copy-plane-issues-to-gitlab
    
    * removed imports
    
    * fixed makefile
    
    * Cut down evaluator and checkpoints. Also created new scoring strategy bonus_for_completing_any_of_given_checkpoints for giving 1st checkpoint points if any of given checkpoints are passed

---
+++ workspaces/tasks/pm-copy-plane-issues-to-gitlab/evaluator.py
@@ -0,0 +1,60 @@
+import json
+from typing import List
+
+from scoring import Result, Checkpoint, bonus_for_completing_any_of_given_checkpoints
+from common import get_plane_project_id, make_gitlab_request
+from config import GITLAB_USER
+
+CHECKPOINT_1_POINTS = 1
+CHECKPOINT_2_POINTS = 1
+CHECKPOINT_3_POINTS = 1
+CHECKPOINT_4_POINTS = 1
+
+GITLAB_PROJECT_PATH = f"{GITLAB_USER}/janusgraph"
+
+PROJECT_NAME = "JanusGraph"
+PROJECT_ID = get_plane_project_id(PROJECT_NAME)
+TODO_ISSUE_1 = "Integrate algorithms into existing graph database"
+TODO_ISSUE_2 = "Implement optimization techniques for large datasets"
+NEW_ISSUE_1 = "Research and design new algorithms"
+NEW_ISSUE_2 = "Analyze current query performance bottlenecks"
+
+def check_issue_exists_in_gitlab(issue_name):
+    issues = make_gitlab_request(GITLAB_PROJECT_PATH, "issues")
+    if issues is None:
+        return False
+    for issue in issues.json():
+        if issue["title"] == issue_name:
+            return True
+    return False
+
+def grade_checkpoint1(trajectory):
+    url = "tac/projects/f04e5c07-8fa2-4eaf-a35b-5cacf3f1d025/issues"
+    return url in trajectory
+
+def grade_checkpoint2():
+    return not check_issue_exists_in_gitlab(TODO_ISSUE_1) and not check_issue_exists_in_gitlab(TODO_ISSUE_2)
+
+def grade_checkpoint3():
+    return check_issue_exists_in_gitlab(NEW_ISSUE_1)
+
+def grade_checkpoint4():
+    return check_issue_exists_in_gitlab(NEW_ISSUE_2)
+
+def grade_checkpoints(trajectory="") -> Result:
+    checkpoints: List[Checkpoint] = []
+    result = Result(checkpoints, bonus_for_completing_any_of_given_checkpoints([3, 4]))
+ 
+    passed1 = grade_checkpoint1(trajectory)
+    checkpoints.append(Checkpoint(CHECKPOINT_1_POINTS, CHECKPOINT_1_POINTS * int(passed1)))
+
+    passed2 = grade_checkpoint2()
+    checkpoints.append(Checkpoint(CHECKPOINT_2_POINTS, CHECKPOINT_2_POINTS * int(passed2)))
+
+    passed3 = grade_checkpoint3()
+    checkpoints.append(Checkpoint(CHECKPOINT_3_POINTS, CHECKPOINT_3_POINTS * int(passed3)))
+
+    passed4 = grade_checkpoint4()
+    checkpoints.append(Checkpoint(CHECKPOINT_4_POINTS, CHECKPOINT_4_POINTS * int(passed4)))
+
+    return result
---
+++ workspaces/tasks/pm-copy-plane-issues-to-gitlab/task.md
@@ -0,0 +1,5 @@
+In Plane there open issues in the JanusGraph project that need to be added to gitlab. I want you to add all "In Progress" tasks to gitlab.
+
+The Plane issues page can be accessed at: http://the-agent-company.com:8091/tac/projects/f04e5c07-8fa2-4eaf-a35b-5cacf3f1d025/issues/
+
+The GitLab issues page can be accessed at: http://the-agent-company.com:8929/root/janusgraph/-/issues/?state=all
