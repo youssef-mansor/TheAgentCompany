4b9a3df4efa5fbb0ab9daf0193eb8f819551e9f9
Zora/Zhiruo Wang <zhiruo.zora.wang@gmail.com>
2024-11-06 23:26:43 -0500

    PM/SDE task: prepare for meeting with customers (#578)
    * add task files
    
    * fix typo in evaluator
    
    * fix import error
    
    * change get chat function
    
    * refine task desc
    
    * refine evaluator
    
    * Update workspaces/tasks/pm-prepare-meeting-with-customers/evaluator.py
    
    Co-authored-by: Boxuan Li <liboxuan@connect.hku.hk>
    
    * Update workspaces/tasks/pm-prepare-meeting-with-customers/evaluator.py
    
    Co-authored-by: Boxuan Li <liboxuan@connect.hku.hk>
    
    * Update workspaces/tasks/pm-prepare-meeting-with-customers/evaluator.py
    
    Co-authored-by: Boxuan Li <liboxuan@connect.hku.hk>
    
    ---------
    
    Co-authored-by: Boxuan Li <liboxuan@connect.hku.hk>

---
+++ workspaces/tasks/pm-prepare-meeting-with-customers/checkpoints.md
@@ -0,0 +1,19 @@
+# Checkpoints
+
+## Checkpoint 1 (1 point)
+Check if the agent visits the openhands evaluation page with all supported tasks (http://the-agent-company.com:8929/root/openhands/-/tree/main/evaluation?ref_type=heads)
+
+## Checkpoint 2 (1 point)
+Check if the presentation file '/workspace/openhands_intro.pptx' exists
+
+## Checkpoint 3 (1 point)
+Check if one slide in the presentation introduces all Software Engineering tasks
+
+## Checkpoint 4 (1 point)
+Check if one slide in the presentation introduces all Web Browsing tasks
+
+## Checkpoint 5 (1 point)
+Check if one slide in the presentation introduces all Misc. Assistance tasks
+
+## Checkpoint 6 (1 point)
+Check if the agent sends the file path to Huang Jie on RocketChat
---
+++ workspaces/tasks/pm-prepare-meeting-with-customers/evaluator.py
@@ -0,0 +1,105 @@
+import os
+import pptx
+import requests
+from typing import List
+from common import create_rocketchat_client
+from common import get_rocketchat_personal_chat_history
+from scoring import Result, Checkpoint, bonus_for_completing_any
+
+# Initialize RocketChat client
+rocket = create_rocketchat_client()
+
+
+def grade_checkpoint1(trajectory):
+    """Check if the agent visits the openhands evaluation page with all supported tasks."""
+    return "root/openhands/-/tree/main/evaluation" in trajectory
+
+
+def grade_checkpoint2():
+    return os.path.exists('/workspace/openhands_intro.pptx')
+
+
+def get_all_texts(slide):
+    """Obtain all text content from the slide."""
+    texts = []
+    
+    for shape in slide.shapes:
+        if shape.has_text_frame:
+            text = shape.text_frame.text
+            texts.append(text.lower())
+    
+    return ' '.join(texts)
+
+
+def grade_checkpoint3(text: str):
+    keywords = [
+        "software engineer",
+        "swe-bench", "humanevalfix",
+        "bird", "biocoder", "ml-bench",
+        "apibench", "toolqa", "aiderbench"
+    ]
+    return all([k in text for k in keywords])
+
+def grade_checkpoint4(text: str):
+    keywords = [
+        "web browsing",
+        "webarena", "miniwob",
+    ]
+    return all([k in text for k in keywords])
+
+def grade_checkpoint5(text: str):
+    keywords = [
+        "misc", "assistance",
+        "gaia", "gpqa", "agentbench",
+        "mint", "eda", "proofwriter"
+    ]
+    return all([k in text for k in keywords])
+
+
+def grade_content():
+    slide_path = '/workspace/openhands_intro.pptx'
+    if not os.path.exists(slide_path):
+        return False, False, False
+    se, web, misc = "", "", ""
+    prs = pptx.Presentation(slide_path)
+    for slide in prs.slides:
+        slide_texts = get_all_texts(slide)
+        if "software engineer" in slide_texts:
+            se += slide_texts
+        elif "web browsing" in slide_texts:
+            web += slide_texts
+        elif "misc" in slide_texts:
+            misc += slide_texts
+    
+    passed3 = grade_checkpoint3(se)
+    passed4 = grade_checkpoint4(web)
+    passed5 = grade_checkpoint5(misc)
+    return passed3, passed4, passed5
+        
+def grade_checkpoint6():
+    chat_history = get_rocketchat_personal_chat_history(rocket, "Huang Jie")
+    return 'openhands_intro.pptx' in ' '.join(chat_history)
+
+
+def grade_checkpoints(trajectory="") -> Result:
+    checkpoints: List[Checkpoint] = []
+    result = Result(checkpoints, bonus_for_completing_any)
+
+    passed1 = grade_checkpoint1(trajectory)
+    checkpoints.append(Checkpoint(1, int(passed1)))
+
+    passed2 = grade_checkpoint2()
+    checkpoints.append(Checkpoint(1, int(passed2)))
+
+    passed3, passed4, passed5 = grade_content()
+    checkpoints.append(Checkpoint(1, int(passed3)))
+    checkpoints.append(Checkpoint(1, int(passed4)))
+    checkpoints.append(Checkpoint(1, int(passed5)))
+
+    passed6 = grade_checkpoint6()
+    checkpoints.append(Checkpoint(1, int(passed6)))
+
+    return result
+
+
+
