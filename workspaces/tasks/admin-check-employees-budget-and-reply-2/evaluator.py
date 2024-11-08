5b4efcbf9f16a8fe77f05d00070319d2c6c70e56
Mengxue Bao <675966327@qq.com>
2024-11-07 23:18:04 -0800

    Administrative Specialist Task: check employee's budget and reply 2 (#569)
    * add task 414
    
    * refine evaluator
    
    * Update workspaces/tasks/admin-check-employees-budget-and-reply-2/evaluator.py
    
    Co-authored-by: Yufan Song <33971064+yufansong@users.noreply.github.com>
    
    * Update task.md
    
    * Update evaluator.py
    
    * Update evaluator.py
    
    * Update task.md
    
    * Update task.md
    
    * Update evaluator.py
    
    ---------
    
    Co-authored-by: baomengxue <baomengxue@bytedance.com>
    Co-authored-by: Yufan Song <33971064+yufansong@users.noreply.github.com>

---
+++ workspaces/tasks/admin-check-employees-budget-and-reply-2/evaluator.py
@@ -0,0 +1,61 @@
+
+from common import *
+from scoring import Result, Checkpoint, bonus_for_completing_final
+
+from typing import List
+
+rocket = create_rocketchat_client()
+ 
+
+def checkpoint1():
+    passed = True
+    for name in ['mike_chen', 'huang_jie', 'chen_xinyi', 'sophia_rodriguez']:
+        passed = passed and bool(get_rocketchat_personal_chat_history(rocket, name))
+    return passed
+
+def checkpoint2(trajectory):
+    return "dir=/Documents/Administrative%20Specialist" in trajectory
+
+def checkpoint3():
+    """
+    Sophia Rodriguez: $69+$99
+    Huang Jie: $899
+    Mike Chen:  $1,799
+    """
+    balance_dict = {
+        'Sophia Rodriguez': '$168',
+        'Huang Jie': '$899',
+        'Mike Chen': '$1799'
+    }
+    for key, value in balance_dict.items():
+        is_value = evaluate_chat_history_with_llm(rocket, key, value)
+        if not is_value:
+              return False
+
+    state_dict = {
+        'Sophia Rodriguez': 'under budget',
+        'Huang Jie': 'under budget',
+        'Mike Chen': 'exceed budget'
+    }
+    for key, value in state_dict.items():
+        is_value = evaluate_chat_history_with_llm(rocket, key, value)
+        if not is_value:
+              return False
+
+    return True
+
+
+def grade_checkpoints(trajectory=''):
+    checkpoints: List[Checkpoint] = []
+    result = Result(checkpoints, bonus_for_completing_final)
+
+    passed1 = checkpoint1()
+    checkpoints.append(Checkpoint(1, int(passed1)))
+
+    passed2 = checkpoint2(trajectory)
+    checkpoints.append(Checkpoint(1, 1 * int(passed2)))
+
+    passed3 = checkpoint3()
+    checkpoints.append(Checkpoint(2, 2 * int(passed3)))
+
+    return result
---
+++ workspaces/tasks/admin-check-employees-budget-and-reply-2/task.md
@@ -0,0 +1,11 @@
+1. Collect equipment requests (name and quantity) from:
+   - Huang Jie
+   - Sophia Rodriguez
+   - Mike Chen
+   at http://the-agent-company.com:3000/home
+
+2. Retrieve equipment prices from Nextcloud (https://ogma.lti.cs.cmu.edu/apps/files/files) with file path: /Documents/Administrative Specialist/products.pdf
+   
+3. Request their total budgets and expenses from the first half of this month from Chen Xinyi
+
+4. Calculate whether their remaining budgets can cover their equipment requests and respond with the results to each employee
