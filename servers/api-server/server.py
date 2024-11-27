ccf216c8c6107d4a1f79cfe4f284728e2925f004
Zhe Su <360307598@qq.com>
2024-11-27 03:02:49 -0500

    [Infra] Add sotopia-redis health check (#810)
    * add sotopia-redis health check
    
    * add redis check in rocketchat healthcheck
    
    * fix bug
    
    ---------
    
    Co-authored-by: yufansong <yufansong98@gmail.com>

--- servers/api-server/server.py
+++ servers/api-server/server.py
@@ -90,14 +90,26 @@ def healthcheck_gitlab():
 
 @app.route('/api/healthcheck/rocketchat', methods=['GET'])
 def healthcheck_rocketchat():
-    code, msg = check_url("http://localhost:3000")
-    return jsonify({"message":msg}), code
+    rocketchat_code, rocketchat_msg = check_url("http://localhost:3000")
+    redis_code, redis_msg = check_url("http://localhost:6379")
+    message = {
+        "rocketchat_msg": rocketchat_msg,
+        "redis_msg": redis_msg,
+    }
+    code = 200 if redis_code == 200 and rocketchat_code == 200 else 500
+
+    return jsonify({"message": message, "redis": redis_code == 200, "rocketchat": rocketchat_code == 200}), code
 
 @app.route('/api/healthcheck/plane', methods=['GET'])
 def healthcheck_plane():
     code, msg = login_to_plane()
     return jsonify({"message":msg}), code
     
+
+@app.route('/api/healthcheck/redis', methods=['GET'])
+def healthcheck_redis():
+    code, msg = check_url("http://localhost:6379")
+    return jsonify({"message":msg}), code
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=2999)
